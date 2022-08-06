6e9a168e72faf0aa74949a2925d2de62de0ceea6
Mackleyn <105865794+HMACKLEYN@users.noreply.github.com>
2022-08-05 20:20:55 -0500

    Add files via upload

---
+++ MI PRIMERA APP/script.js
@@ -0,0 +1,8 @@
+//Ejemplo algoritmo sencillo
+console.log("Algoritmo para sumar dos numeros");
+console.log("Paso a paso:");
+console.log("1.Obtener el primer numero a sumar");
+console.log("2.Obtener el segundo numero a sumar");
+console.log("3.Adicionar la cantidad del segundo numero al primer numero");
+console.log("4.Almacenar el resultado");
+console.log("5.Mostrar el resultado");
